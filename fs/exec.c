HavocGemini/soda_xiaomi_msm8996

fpc1020: add ifdefs around wakeup boost functions
Signed-off-by: xNombre <kartapolska@gmail.com>

fs/exec.c
@@ -66,7 +66,9 @@
 
 #include <trace/events/sched.h>
 
+#ifdef CONFG_FINGERPRINT_FPC1020_TA
 #include <linux/fpc1020.h>
+#endif
 
 int suid_dumpable = 0;
 
@@ -1569,8 +1571,10 @@ static int do_execve_common(struct filename *filename,
 	if (retval < 0)
 		goto out;
 
+#ifdef CONFG_FINGERPRINT_FPC1020_TA
 	if (unlikely(!strcmp(filename->name, FP_HAL_BIN)))
 		atomic_set(&fp_hal_pid, current->pid);
+#endif
 
 	if (is_su && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;

nicolosponziello/android_kernel_xiaomi_msm8996

fs/exec.c
@@ -66,7 +66,9 @@
 
 #include <trace/events/sched.h>
 
+#ifdef CONFIG_FINGERPRINT_FPC1020_TA
 #include <linux/fpc1020.h>
+#endif
 
 int suid_dumpable = 0;
 
@@ -1576,13 +1578,17 @@ static int do_execve_common(struct filename *filename,
 	if (retval < 0)
 		goto out;
 
+
 	if (is_su && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
 	}
 
+
+#ifdef CONFIG_FINGERPRINT_FPC1020_TA
 	if (unlikely(!strcmp(filename->name, FP_HAL_BIN)))
 		atomic_set(&fp_hal_pid, current->pid);
+#endif
 
 
 	/* execve succeeded */